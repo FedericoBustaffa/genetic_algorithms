129c6e65473c3f5da5a4fa31547843f2dd462663
FedericoBustaffa <kiko.bustaffa@gmail.com>
2024-04-26 14:52:48 +0200

    maze

--- maze.hpp
+++ maze.hpp
@@ -1,12 +1,46 @@
 #ifndef MAZE_HPP
 #define MAZE_HPP
 
+#include <cstddef>
+#include <ostream>
+#include <random>
+#include <vector>
+
 class maze
 {
   public:
-    maze() {}
+    maze(size_t height, size_t width) : m_height(height), m_width(width), engine(rd()) {}
+
+    size_t get_height() const { return m_height; }
+
+    size_t get_width() const { return m_width; }
+
+    void generate(size_t x, size_t y) {}
 
   private:
+    size_t m_height, m_width;
+
+    class cell
+    {
+      public:
+        cell(size_t x, size_t y) : x(x), y(y) {}
+
+      private:
+        size_t x;
+        size_t y;
+    };
+
+    std::vector<std::vector<cell>> m_maze;
+    std::vector<bool> m_grid;
+
+    std::random_device rd;
+    std::mt19937 engine;
 };
+
+inline std::ostream& operator<<(std::ostream& os, const maze& m)
+{
+    os << m.get_height() << ", " << m.get_width();
+    return os;
+}
 
 #endif  // !MAZE_HPP
--- maze_solver.cpp
+++ maze_solver.cpp
@@ -1,7 +1,12 @@
 #include <iostream>
 
+#include "maze.hpp"
+
 int main(int argc, const char** argv)
 {
-    std::cout << "Maze resolver not implemented yet" << std::endl;
+    maze m(10, 10);
+    m.generate(0, 0);
+    std::cout << m << std::endl;
+
     return 0;
 }
