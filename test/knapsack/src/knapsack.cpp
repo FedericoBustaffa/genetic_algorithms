b83c0aad9f145ef33f5657985c5306bc2e10f772
FedericoBustaffa <kiko.bustaffa@gmail.com>
2024-05-11 20:32:01 +0200

    format

--- test/knapsack/src/knapsack.cpp
+++ test/knapsack/src/knapsack.cpp
@@ -17,7 +17,7 @@
 
 struct item
 {
-    item(int64_t value, int64_t weight) : value(value), weight(weight){};
+    item(int64_t value, int64_t weight) : value(value), weight(weight) {};
 
     int64_t value;
     int64_t weight;
